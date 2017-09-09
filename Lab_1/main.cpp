03d5d15ba4b85e05fda52ca5b28d5b928e5da9bf
Starovoytov Alexey <alexey.starovoytov@travelline.ru>
2017-09-09 07:48:09 +0000

    Добавлена поддержка вывода русских символов. Добавлена проверка на корректное количество параметров входной строки.

---
+++ Lab_1/main.cpp
@@ -0,0 +1,27 @@
+﻿#include "stdafx.h"
+using namespace std;
+
+const int ARG_COUNT = 4;
+
+void PrintError(wstring msg)
+{
+	wcout << L"Ошибка: " << msg << L"\n";
+}
+
+void UseUTF16()
+{
+	_setmode(_fileno(stdout), _O_U16TEXT);
+	_setmode(_fileno(stdin), _O_U16TEXT);
+	_setmode(_fileno(stderr), _O_U16TEXT);
+}
+
+int wmain(int argc, wchar_t *argv[])
+{
+	UseUTF16();
+	if (argc != ARG_COUNT)
+	{
+		PrintError(L"Укажите длины сторон в качестве параметров. Формат ввода: triangle.exe a b c");
+		return 1;
+	}
+	return 0;
+}
