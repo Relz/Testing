8f071e0e9eeb9462136f5f211d84812ea005d679
Starovoytov Alexey <alexey.starovoytov@travelline.ru>
2017-09-21 13:04:00 +0000

    Добавлена безопасность к возникновению исключения range_error от from_bytes.

--- Lab_2/UTF8.cpp
+++ Lab_2/UTF8.cpp
@@ -2,7 +2,15 @@
 
 std::wstring ToUTF8(const std::string & str)
 {
-	std::wstring_convert<std::codecvt_utf8_utf16<wchar_t>> converter;
-	std::wstringstream strm(converter.from_bytes(str));
-	return strm.str();
+	try
+	{
+		std::wstring_convert<std::codecvt_utf8_utf16<wchar_t>> converter;
+		std::wstringstream strm(converter.from_bytes(str));
+		return strm.str();
+	}
+	catch (std::exception & ex)
+	{
+		std::cout << ex.what() << "\n";
+		return std::wstring();
+	}
 }
