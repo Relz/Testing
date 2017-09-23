a801777512089ce329c5ae7633a2a376d88ac90c
Starovoytov Alexey <alexey.starovoytov@travelline.ru>
2017-09-23 17:00:58 +0000

    Функция Print сделана доступной извне

--- Lab_2/Print.h
+++ Lab_2/Print.h
@@ -1,5 +1,7 @@
 #pragma once
 
+void Print(const std::string & msg);
+void Print(const std::string & msg, std::wofstream & outFile);
 void Println(const std::string & msg);
 void Println(const std::string & msg, std::wofstream & outFile);
 void PrintlnError(const std::string & msg);
